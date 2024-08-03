a10a3b960c49d1b7cde0d5c5b316cdc2b8878d65
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2024-08-03 19:15:45 +0300

    Display different UI for different senders

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _fireStore = FirebaseFirestore.instance;
+User? loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -16,7 +17,6 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextFieldController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  late User loggedInUser;
   late String messageText;
 
   @override
@@ -79,8 +79,9 @@ class _ChatScreenState extends State<ChatScreen> {
                       messageTextFieldController.clear();
                       _fireStore.collection('messages').add(
                         {
-                          'sender': loggedInUser.email,
+                          'sender': loggedInUser!.email,
                           'text': messageText,
+                          'time': FieldValue.serverTimestamp(),
                         },
                       );
                     },
@@ -105,7 +106,10 @@ class MessageStream extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _fireStore.collection('messages').snapshots(),
+      stream: _fireStore
+          .collection('messages')
+          .orderBy('time', descending: true)
+          .snapshots(),
       builder: (context, AsyncSnapshot snapshot) {
         if (!snapshot.hasData) {
           return const Center(
@@ -119,15 +123,19 @@ class MessageStream extends StatelessWidget {
         for (var message in messages) {
           final messageText = message.data()['text'];
           final messageSender = message.data()['sender'];
+          final currentUser = loggedInUser!.email;
+
           final messageWidget = MessageBubble(
             text: messageText,
             sender: messageSender,
+            isMe: currentUser == messageSender,
           );
 
           messageBubbles.add(messageWidget);
         }
         return Expanded(
           child: ListView(
+            reverse: true,
             padding:
                 const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
             children: messageBubbles,
@@ -141,14 +149,16 @@ class MessageStream extends StatelessWidget {
 class MessageBubble extends StatelessWidget {
   final text;
   final sender;
-  const MessageBubble({super.key, this.text, this.sender});
+  final bool isMe;
+  const MessageBubble({super.key, this.text, this.sender, required this.isMe});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: <Widget>[
           Text(
             sender,
@@ -158,15 +168,27 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: isMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
               child: Text(
                 text,
-                style: const TextStyle(color: Colors.white),
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                ),
               ),
             ),
           ),
